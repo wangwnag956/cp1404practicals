e4be84a990a935456b25f70068c5b1e0f4e51c38
wangwnag956 <19953302713@163.com>
2025-07-06 10:07:15 +0800

    Update languages.py
    Import the content defined just now and create three objects of the "programminglanguage" class.

---
+++ prac_06/languages.py
@@ -0,0 +1,8 @@
+from programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)  # Test __str__
