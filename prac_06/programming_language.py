a2fe0bf10f51bcec56ecf08a5aad73ce71b8b685
wangwnag956 <19953302713@163.com>
2025-07-06 09:59:33 +0800

    Update programming_language.py
    Define and store relevant information and create parameters for invocation and display.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,7 @@
+class ProgrammingLanguage:
+
+def __init__(self, name, typing, reflection, year): 
+    self.name = name
+    self.typing = typing
+    self.reflection = reflection
+    self.year = year
