0ae5c5cb1a82b756df3609a0fab98f52d4add609
wangwnag956 <19953302713@163.com>
2025-07-20 02:41:33 +0800

    Update box_layout_demo.py
    According to the requirements, clicking on "greet" will display "hello", and clicking on "clear" will clear the input and output.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -7,6 +7,14 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+        
+   def handle_greet(self):
+        name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {name}"
+
+   def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
 
 
 BoxLayoutDemo().run()
