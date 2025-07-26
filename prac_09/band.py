52f8c8de7be63c7bcd5d4f7b5d138a53222e4121
wangwnag956 <19953302713@163.com>
2025-07-27 02:37:23 +0800

    Update band.py
    Added one musician and instructed each musician to play

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,9 +1,18 @@
 class Band:
     """A band with a name and a list of musicians."""
 
-    def __init__(self, name):
-        self.name = name
-        self.musicians = []
+ def __init__(self, name):
+    self.name = name
+    self.musicians = []
 
-    def __str__(self):
-        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
+ def __str__(self):
+    return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})" 
+
+ def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Tell each musician in the band to play."""
+        for musician in self.musicians:
+            musician.play()
