b3e6c848025509f807a8d921d2f996859bed1500
wangwnag956 <19953302713@163.com>
2025-07-27 02:17:42 +0800

    Update unreliable_car.py
    Drive the car only if a random number is less than the car's reliability.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,14 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar instance."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+  def drive(self, distance):
+        """
+        Drive the car only if a random number is less than the car's reliability.
+        Return the actual distance driven.
+        """
+        random_chance = random.uniform(0, 100)
+        if random_chance < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0  # The car did not drive
