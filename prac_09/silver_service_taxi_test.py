b803fe80131cbeba47573151de9d0517539917b7
wangwnag956 <19953302713@163.com>
2025-07-27 02:32:25 +0800

    Update silver_service_taxi_test.py
    Use assert for verification (expected result: 18 * 4.92 + 4.50 = 88.06) and retest

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -9,3 +9,19 @@ def main():
   # Obtain ticket prices and print them
   fare = fancy_taxi.get_fare()
     print(f"Fare for 18km = ${fare:.2f}")
+
+# Use assert for verification (expected: 18 * 4.92 + 4.50 = 88.06)
+    expected_fare = 18 * 4.92 + 4.50
+    assert abs(fare - expected_fare) < 0.01, "Fare calculation error!" 
+
+#retest
+    fancy_taxi.start_fare()
+    fancy_taxi.drive(2)
+    fare = fancy_taxi.get_fare()
+    print(f"Fare for 2km = ${fare:.2f}")
+
+expected_fare = 2 * 4.92 + 4.50
+    assert abs(fare - expected_fare) < 0.01, "Fare calculation error!"
+
+if __name__ == "__main__":
+    main()
