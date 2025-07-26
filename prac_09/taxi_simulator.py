529edbd1c9baea62d0b8f0598cf2e61ecfb6a89d
wangwnag956 <19953302713@163.com>
2025-07-27 02:42:47 +0800

    Update taxi_simulator.py
    Make recursive calls to itself and pass the updated state

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -16,3 +16,41 @@ def run_simulator(taxis, current_taxi=None, total_bill=0.0):
         print("Taxis are now:")
         display_taxis(taxis)
         return  
+
+
+    elif choice == 'c':
+        display_taxis(taxis)
+        try:
+            taxi_choice = int(input("Choose taxi: "))
+            if 0 <= taxi_choice < len(taxis):
+                current_taxi = taxis[taxi_choice]
+            else:
+                print("Invalid taxi choice")
+        except ValueError:
+            print("Invalid taxi choice")
+
+    elif choice == 'd':
+        if current_taxi is None:
+            print("You need to choose a taxi before you can drive")
+            print(f"Bill to date: ${total_bill:.2f}")
+        else:
+            try:
+                distance = float(input("Drive how far? "))
+                if distance < 0:
+                    print("Distance must be >= 0")
+                else:
+                    current_taxi.start_fare()
+                    current_taxi.drive(distance)
+                    fare = current_taxi.get_fare()
+                    total_bill += fare
+                    print(f"Your {current_taxi.name} trip cost you ${fare:.2f}")
+                    print(f"Bill to date: ${total_bill:.2f}")
+            except ValueError:
+                print("Invalid distance")
+
+    else:
+        print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+
+# Make recursive calls to itself and pass the updated state
+ run_simulator(taxis, current_taxi, total_bill)
