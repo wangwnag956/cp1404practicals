76c515e54f326edf7a746db7ada3431aed9b2a49
wangwnag956 <19953302713@163.com>
2025-07-28 13:23:00 +0800

    Update test.py
    The code has been written as per the requirements.

---
+++ prac_10/test.py
@@ -0,0 +1,20 @@
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return " ".join([s] * n)
+
+
+def is_long_word(word, length=5):
+    """
+    Determine if the word is as long or longer than the length passed in
+    >>> is_long_word("not")
+    False
+    >>> is_long_word("supercalifrag")
+    True
+    >>> is_long_word("Python", 6)
+    True
+    """
+    return len(word) >= length
